0ecad291cc72ee1db326a6d44763f070ce19d969
Sabbir-123 <ahmedsabbir2013@gmail.com>
2023-08-01 00:27:41 +0600

    practice

--- src/pages/api/news.js
+++ src/pages/api/news.js
@@ -1,7 +1,34 @@
-const handler = (req, res)=>{
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nextJSPractice:63UkjGJYACKoXRJc@cluster0.j4x9j8z.mongodb.net/?retryWrites=true&w=majority";
 
-    res.send({message:"Successfull"})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run(req, res) {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const newCollection = await client.db("news_portal").collection("news");
+if(req.method === "GET"){
+    const news  = await newCollection.find({}).toArray();
+    res.send({
+        message: "Success",
+        data: news,
+        statusCode :200
+    })
+}
+} finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 
-export default handler
+export default run;
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ const HomePage = ({allNews}) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <DynamicBanner />
-      <AllNews allNews={data}/>
+      <AllNews allNews={allNews}/>
     </>
   );
 };
@@ -38,11 +38,11 @@ HomePage.getLayout = function getLayout(page) {
 
 
 export const getServerSideProps = async ()=>{
-  const res = await fetch('http://localhost:5000/news');
+  const res = await fetch('http://localhost:3000/api/news');
   const data = await res.json();
   return {
     props:{
-      allNews:data,
+      allNews:data.data,
     }
   }
 }
